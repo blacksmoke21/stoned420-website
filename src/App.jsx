504ca97392c4ca2d9580c8880f7c89ee0e03c0b6
ultranoob3 <ultranoob3@pm.me>
2025-05-15 10:39:44 +0530

    added Nostr and LN Address

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import Header from "./components/Header";
 import Home from "./components/Home";
-import ThemeToggle from "./components/ThemeToggle";
 
 function App() {
   return (
     <div>
-      {/* <Header /> */}
-      <ThemeToggle />
       <Home />
     </div>
   );
